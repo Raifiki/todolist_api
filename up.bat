e09ef71b6e4a3900d618b3dea62aa6461067c952
Raifikie <leonard_weiss@web.dem>
2024-07-05 16:23:30 +0200

    open chrome in up.bat

--- up.bat
+++ up.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m "%*"
 git push
-chrome "https://www.pythonanywhere.com/user/Leo3005/consoles/34563693/"
+start chrome "https://www.pythonanywhere.com/user/Leo3005/consoles/34563693/"
